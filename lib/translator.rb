8ef87fd387436934cecb32e4fb97ae9ad6890d50
Mykle jones <mykle.j91@gmail.com>
2019-08-19 20:04:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,25 @@
-# require modules here
-
-def load_library
-  # code goes here
+require "yaml"
+ 
+def load_library(emo_icon)
+emo_icon = YAML.load_file("./lib/emoticons.yml")
+emoticons =  {
+  'get_meaning' => {},
+  'get_emoticon' => {}
+}
+emo_icon.each do |key, val|
+  emoticons['get_emoticon'][val[0]] = emo_icon[key][1]
+    emoticons['get_meaning'][val[1]] =key
 end
+emoticons
+  end
+  
 
-def get_japanese_emoticon
-  # code goes here
+
+  
+
+
+def get_japanese_emoticon(emo_icon,emoticons)
+ load_library(emo_icon)
 end
 
 def get_english_meaning
